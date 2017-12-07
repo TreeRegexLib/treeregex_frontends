679a5bd1283b78f6168bc919ba84c6ebce0f2f6e
Ben Mehne <bmehne@gmail.com>
2017-12-06 17:15:29 -0800

    Updated python implementaiton to support tokens, labels, and dropping whitespace

--- python/python_to_sexp.py
+++ python/python_to_sexp.py
@@ -1,37 +1,81 @@
 import sys
 import asttokens, ast
+from token import tok_name
+import string
+
+ESCAPE_STR="\\"
+OPEN_P="(%"
+CLOSE_P="%)"
+TOKEN_FLAGS=["-t"]
+LABEL_FLAGS=["-l"]
+WHITESPACE_FLAGS=["-nws"]
+TO_ESCAPE=[OPEN_P, CLOSE_P]
+FLAGS=TOKEN_FLAGS+ LABEL_FLAGS+ WHITESPACE_FLAGS
+INCLUDE_TOKENS=any(e in sys.argv for e in TOKEN_FLAGS)
+INCLUDE_LABELS=any(e in sys.argv for e in LABEL_FLAGS)
+REMOVE_WHITESPACE=any(e in sys.argv for e in WHITESPACE_FLAGS)
+
+def safe_min(iterable, default=None):
+	try:
+		return min(iterable)
+	except ValueError:
+		return default
 
 def find_subsexps(ast_, files):
+	height = 0
 	for node in ast.walk(ast_.tree):
 		b,e =ast_.get_text_range(node)
-		if b != e:
-			yield b,e
+		if b==0 and e==0:
+			continue
+		yield b,e,height, type(node).__name__
+		if INCLUDE_TOKENS:
+			for t in ast_.get_tokens(node, include_extra=True):
+				yield t[6], t[7], height, tok_name[t[0]]
+		height+=1
+
+def escape(s):
+	index = safe_min(list(filter(lambda a:a[0]>=0, ((s.find(e),e) for e in TO_ESCAPE))), default=(-1,None))
+	out = ""
+	while index[0] != -1:
+		out+=s[:index[0]]+ESCAPE_STR+index[1]
+		s = s[index[0]+len(index[1]):]
+		index = safe_min(list(filter(lambda a:a[0]>=0, ((s.find(e),e) for e in TO_ESCAPE))), default=(-1,None))
+	out+=s
+	if REMOVE_WHITESPACE:
+		return "".join(filter(lambda a : a not in string.whitespace, out))
+	return out
 
 def main():
-	with open(sys.argv[1]) as f:
+	files = list(filter(lambda a: a not in FLAGS, sys.argv[1:]))
+	try:
+		for f in files:
+			do_file(f)
+	except FileNotFoundError:
+		print("File not found:", f)
+		print("Did you mean one of the flags:", " ".join(FLAGS))
+
+def do_file(fname):
+	with open(fname) as f:
 		source = f.read()
 	ast_ = asttokens.ASTTokens(source, parse=True)
 
-	offsets = list(find_subsexps(ast_, [sys.argv[1]]))
-	starts = list(k for k,v in offsets)
-	ends = list(v for k,v in offsets)
+	offsets = list(find_subsexps(ast_, [fname]))
 
-	joined = list( (t, "(%") for t in starts) + list( (t, "%)") for t in ends)
+	if INCLUDE_LABELS:
+		joined = list( (k, h, OPEN_P+t+': ') for k,v,h,t in offsets) + list( (v, -h, CLOSE_P) for k,v,h,t in offsets)
+	else:
+		joined = list( (k, h, OPEN_P) for k,v,h, t in offsets) + list( (v, -h, CLOSE_P) for k,v,h,t in offsets)
 	joined.sort()
-	print(joined)
 
-	with open(sys.argv[1]+".sexp", "w") as f:
+	with open(fname+".sexp", "w") as f:
 		out_source = ""
 		last = 0
-		for p, s in joined:
-			out_source += source[last:p]+s
+		for p, _, s in joined:
+			out_source += escape(source[last:p])+s
 			last = p
 		out_source += source[last:]
 		f.write(out_source)
-	print("BEFORE------------\n")
-	print(source)
-	print("AFTER------------\n")
-	print(out_source)
+	#print(out_source, end='')
 
-main()
-
+if __name__ == "__main__":
+	main()
