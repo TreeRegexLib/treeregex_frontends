d5d64fd66af9bc3cf149bf21b3b7fa54f11d05b9
Ben Mehne <bmehne@gmail.com>
2017-12-06 17:36:40 -0800

    Added support for 2 languages, in preliminary stages, and flags for whitespace, label, and token handling

--- cpp/README.md
+++ cpp/README.md
@@ -1,3 +1,12 @@
 # C/C++ frontend
 
-This frontend converts C/C++ to serialized tree form.  To run it, you must have the libclang python bindings installed.  The script must be run with Python2 for the libclang version specified in the requirements.txt file (later versions work with Python 3).
+This frontend converts C/C++/ObjectiveC to serialized tree form.  To run it, you must have the libclang python bindings installed (see `requirements.txt`).  The script must be run with Python2 for the libclang version specified in the requirements.txt file (later versions work with Python 3).
+
+It supports the following flags:
+
+	`-nws` which removes whitespace
+	`-l` which labels each subtree
+	`-t` which does token-level subtrees (instead of just grammar level subtrees)
+	`--language=*lang*` which treats the source file as *lang* language (supported: c++, c, objective-c).
+
+NOTE: objective-c will probably require a change to the include paths in the source file (it has different install paths on different machines)
--- cpp/cpp_to_sexp.py
+++ cpp/cpp_to_sexp.py
@@ -1,58 +1,97 @@
+from __future__ import print_function
 import sys
 import clang.cindex
 import pprint
+import string
 
-CXX_INCLUDES="/usr/include", "/usr/include/c++"
+ESCAPE_STR="\\"
+OPEN_P="(%"
+CLOSE_P="%)"
+TOKEN_FLAGS=["-t"]
+LABEL_FLAGS=["-l"]
+WHITESPACE_FLAGS=["-nws"]
+LANGUAGE_FLAGS=["--lang=c++", "--lang=c", "--lang=objective-c"]
+TO_ESCAPE=[OPEN_P, CLOSE_P]
+FLAGS=TOKEN_FLAGS+ LABEL_FLAGS+ WHITESPACE_FLAGS
+INCLUDE_TOKENS=any(e in sys.argv for e in TOKEN_FLAGS)
+INCLUDE_LABELS=any(e in sys.argv for e in LABEL_FLAGS)
+REMOVE_WHITESPACE=any(e in sys.argv for e in WHITESPACE_FLAGS)
+LANG=next((x for x in sys.argv if x in LANGUAGE_FLAGS), "--lang=c++").split("=")[1]
 
-def find_subsexps(node, files):
-	#print(node.kind, node.displayname, node.extent)
+INCLUDES={'c++':["/usr/include", "/usr/include/c++"], 'c':['/usr/include/'], 'objective-c':['/usr/include/']}
+
+def safe_min(iterable, default=None):
+	try:
+		return min(iterable)
+	except ValueError:
+		return default
+
+
+def find_subsexps(node, files, tu):
+	out = _find_subsexps(node, files)
+	if INCLUDE_TOKENS:
+		inf = float('inf')
+		for t in tu.get_tokens(extent = node.extent):
+			out.append((t.extent.start.offset, t.extent.end.offset, inf, t.kind.name))
+	return out
+
+def _find_subsexps(node, files, height=0):
 	if node.extent.start.file is not None and node.extent.start.file.name in files:
-		ret = [(node.extent.start.offset, node.extent.end.offset)]
+		ret = [(node.extent.start.offset, node.extent.end.offset, height, node.kind.name)]
 	else:
 		ret = []
 	children = list(node.get_children())
 	for c in children:
-		ret.extend(find_subsexps(c, files))
+		ret.extend(_find_subsexps(c, files, height+1))
 	return ret
 
-def main():
+def escape(s):
+	index = safe_min(list(filter(lambda a:a[0]>=0, ((s.find(e),e) for e in TO_ESCAPE))), default=(-1,None))
+	out = ""
+	while index[0] != -1:
+		out+=s[:index[0]]+ESCAPE_STR+index[1]
+		s = s[index[0]+len(index[1]):]
+		index = safe_min(list(filter(lambda a:a[0]>=0, ((s.find(e),e) for e in TO_ESCAPE))), default=(-1,None))
+	out+=s
+	if REMOVE_WHITESPACE:
+		return "".join(filter(lambda a : a not in string.whitespace, out))
+	return out
 
+def main():
+	files = list(filter(lambda a: a not in FLAGS, sys.argv[1:]))
+	try:
+		for f in files:
+			do_file(f)
+	except clang.cindex.TranslationUnitLoadError:
+		print("File not found:", f)
+		print("Did you mean one of the flags:", " ".join(FLAGS))
+
+def do_file(fname):
 	index = clang.cindex.Index.create()
 	args = ['-x','c++']
-	for a in CXX_INCLUDES:
+	for a in INCLUDES[LANG]:
 		args.extend(['-I', a])
-	tu = index.parse(sys.argv[1], args=args)
+	tu = index.parse(fname, args=args)
 
-	diagnostics = list(tu.diagnostics)
-	if len(diagnostics) > 0:
-		print("There were parse errors")
-		pprint.pprint(diagnostics)
+	offsets = list(find_subsexps(tu.cursor, [fname], tu))
 
-	offsets = list(find_subsexps(tu.cursor, [sys.argv[1]]))
-
-	starts = list(k for k,v in offsets)
-	ends = list(v for k,v in offsets)
-
-	#starts.sort()
-	#ends.sort()
-	joined = list( (t, "(%") for t in starts) + list( (t, "%)") for t in ends)
+	if INCLUDE_LABELS:
+		joined = list( (k, h, OPEN_P+t+': ') for k,v,h,t in offsets) + list( (v, -h, CLOSE_P) for k,v,h,t in offsets)
+	else:
+		joined = list( (k, h, OPEN_P) for k,v,h, t in offsets) + list( (v, -h, CLOSE_P) for k,v,h,t in offsets)
 	joined.sort()
-	#print(joined)
 
-	with open(sys.argv[1]) as f:
-		data = f.read()
-	with open(sys.argv[1]+".sexp", "w") as f:
-		out_data = ""
+	with open(fname) as f:
+		source = f.read()
+	with open(fname+".sexp", "w") as f:
+		out_source = ""
 		last = 0
-		for p, s in joined:
-			out_data += data[last:p]+s
+		for p, _, s in joined:
+			out_source += escape(source[last:p])+s
 			last = p
-		out_data += data[last:]
-		f.write(out_data)
-	print("BEFORE------------\n")
-	print(data)
-	print("AFTER------------\n")
-	print(out_data)
+		out_source += source[last:]
+		f.write(out_source)
+	#print(out_source, end='')
 
-main()
-
+if __name__ == "__main__":
+	main()
